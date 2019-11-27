f88c827a3ee8307126f94ab172b7c9024a6377e6
Marandi <Marandi@139.com>
2019-11-27 19:06:19 +0800

    deploy project in docker

---
+++ app/config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    mongodb_url: 'mongodb://db:27017/tinyurl',
+    redis_host: 'cache',
+    redis_port: '6379',
+};
--- config.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-    mongodb_url: 'mongodb://localhost:27017/tinyurl',
-    redis_host: 'localhost',
-    redis_port: '6379',
-};
